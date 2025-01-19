a299ac94ff7f85e58104884ee5487146f2541bdf
Manohar Kakumani <42714264+manoharkakumani@users.noreply.github.com>
2025-01-19 15:05:45 -0500

    Update decode-wa-message.ts

--- src/Utils/decode-wa-message.ts
+++ src/Utils/decode-wa-message.ts
@@ -4,7 +4,7 @@ import { proto } from '../../WAProto'
 import { SignalRepository, WAMessageKey } from '../Types'
 import { areJidsSameUser, BinaryNode, isJidBroadcast, isJidGroup, isJidNewsletter, isJidStatusBroadcast, isJidUser, isLidUser } from '../WABinary'
 
-import { unpadRandomMax16, BufferJSON, unpadRandomMax16 } from './generics'
+import { unpadRandomMax16, BufferJSON } from './generics'
 
 export const NO_MESSAGE_FOUND_ERROR_TEXT = 'Message absent from node'
 
